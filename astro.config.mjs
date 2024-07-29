0337b6d249191f42b0bf558c486738cf68873b1f
Ruben Arakelyan <ruben@arakelyan.uk>
2024-07-29 16:19:11 +0100

    Add support for Docker and deploying to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
+import node from "@astrojs/node";
 import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
 import sitemap from "@astrojs/sitemap";
@@ -9,8 +9,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: cloudflare({
-    imageService: "passthrough"
+  adapter: node({
+    mode: "standalone"
   }),
   site: process.env.NODE_ENV === "development" ? "http://localhost:4321" : "https://www.json-wf.org.uk",
   integrations: [mdx({
